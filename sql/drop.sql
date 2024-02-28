ca7cfaa232d6b3124bb452a670961b6b8bb2eb47
annafyodorova <127510613+annafyodorova@users.noreply.github.com>
2024-02-28 13:43:28 +0300

    Update drop.sql

--- sql/drop.sql
+++ sql/drop.sql
@@ -1,5 +1,4 @@
 drop table if exists video_host_schema.film_copies;
 drop table if exists video_host_schema.storage_info;
-drop table if exists video_host_schema.storage_device;
 drop table if exists video_host_schema.films;
 drop table if exists video_host_schema.clients;
