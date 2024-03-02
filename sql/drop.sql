9013e5d3a058141dfa8aa2e9a9be6ef3489d6c3a
annafyodorova <127510613+annafyodorova@users.noreply.github.com>
2024-03-02 13:44:12 +0300

    Update drop.sql

--- sql/drop.sql
+++ sql/drop.sql
@@ -1,3 +1,4 @@
+drop table if exists video_host_schema.history;
 drop table if exists video_host_schema.film_copies;
 drop table if exists video_host_schema.storage_info;
 drop table if exists video_host_schema.films;
